55b859148303183992c1ae2d974b9868a5cc733f
Yikai Wu <yw267@cs.duke.edu>
2020-09-02 23:21:49 -0400

    Added run file

--- hessian_approx_run_10class.py
+++ hessian_approx_run_10class.py
@@ -4,12 +4,14 @@ from hpb.hessianpacbayes import *
 
 sd_sgd_sol = 'experiment_log/run_1/models/final.pth'
 sd_init = 'experiment_log/run_1/models/epoch0.pth'
-save_path = 'tmp_log/sigma_post/hessian_approx_FC2_600_long.pth'
+save_path = 'tmp_log/sigma_post/hessian_approx_10class_FC2.pth'
 
-test_dirc = '../hessian_eigenspace_overlap/MNIST_TrueBinary/experiments/FC2_20_small_fixlr0.01_pn1'
-hessian_file = 'experiment_log/run_1/MNIST_TrueBinary_FC2_20_small_fixlr0.01_pn1_E-1_UTAU_xxT.eval'
-test_dirc = '../hessian_eigenspace_overlap/MNIST_Binary/experiments/FC2_600_sgd0.01m0.9LS_l1d_pic01_labelpn1_bt100'
-hessian_file = 'experiment_log/run_1/MNIST_Binary_FC2_600_sgd0.01m0.9LS_l1d_pic01_labelpn1_bt100_E-1_UTAU_xxT.eval'
+test_dirc = '../hessian_eigenspace_overlap/MNIST_Exp1/experiments/FC2_fixlr0.01'
+hessian_file = 'experiment_log/run_1/MNIST_Exp1_FC2_fixlr0.01_E-1_UTAU_xxT.eval'
+test_dirc = '../hessian_eigenspace_overlap/MNIST_Exp1/experiments/FC1_600_fixlr0.01'
+hessian_file = 'experiment_log/run_1/MNIST_Exp1_FC1_600_fixlr0.01_E-1_UTAU_xxT.eval'
+test_dirc = '../hessian_eigenspace_overlap/MNIST_Exp1/experiments/FC2_600_fixlr0.01'
+hessian_file = 'experiment_log/run_1/MNIST_Exp1_FC2_600_fixlr0.01_E-1_UTAU_xxT.eval'
 
 def main():
 
@@ -35,10 +37,10 @@ def main():
     HPB.initialize_BRE(mean_prior)
 
     # HPB.optimize_PACB_RMSprop(learning_rate=0.001, epoch_num=3000, lr_decay_mode='step', lr_gamma=0.1, step_lr_decay=1000)
-    HPB.optimize_PACB_RMSprop(learning_rate=0.001, epoch_num=2000, lr_decay_mode='step', lr_gamma=0.1, step_lr_decay=400)
+    HPB.optimize_PACB_RMSprop(learning_rate=0.001, epoch_num=1000, lr_decay_mode='step', lr_gamma=0.1, step_lr_decay=200)
     # HPB.optimize_PACB_RMSprop(learning_rate=0.01, epoch_num=800, lr_decay_mode='exp', lr_gamma=0.1 ** (1/40))
     # exit()
-    HPB.compute_bound(n_monte_carlo_approx=150000, sample_freq=100)
+    HPB.compute_bound(n_monte_carlo_approx=40000, sample_freq=100)
     #print(list(HPB.BRE.sigma_post_.detach().to('cpu').numpy()))
     torch.save(HPB.BRE.sigma_post_.detach().to('cpu'), save_path)
 
